a075af167d06cdad8c82dec26576556a4aed1255
hfirestone <47194485+hfirestone@users.noreply.github.com>
2026-04-18 13:01:47 -0400

    Update testimonials with more info

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -12,21 +12,21 @@ const columns = [
   ],
   [
     {
-      author: "Otis Livingston",
-      quote: "I'm really happy working with Sevag. I started working with him in 2022 and I have trusted him since day one. His professionalism is top notch and his communication is always on point. I know working with him that I have the support needed to be successful. Sevag always helps to make quality decisions for both the short term and long term.",
-    },
-    {
       author: "Chris Manon, Los Angeles Lakers",
       quote: "Sevag has been a reliable and supportive agent, always keeping things on track and prioritizing my best interests. His professionalism stood out, and his visits to Los Angeles showed an extra level of dedication that made me feel supported throughout my first year as a pro.",
+    },
+    {
+      author: "Otis Livingston, Bàsquet Girona",
+      quote: "I'm really happy working with Sevag. I started working with him in 2022 and I have trusted him since day one. His professionalism is top notch and his communication is always on point. I know working with him that I have the support needed to be successful. Sevag always helps to make quality decisions for both the short term and long term.",
     },
   ],
   [
     {
-      author: "Christian Vital",
+      author: "Christian Vital, Derthona Basket",
       quote: "Sevag is an agent that is restless. With many connections in Europe and the states he works relentlessly to make sure his guys have jobs and places them where they can succeed and grow as a person and player.",
     },
     {
-      author: "Chris Jones",
+      author: "Chris Jones, Hapoel Tel Aviv",
       quote: "Sevag discovered me while I was playing in Mongolia. I was just a young player with a dream, and he believed in me when almost no one else did. Since then, he has guided me step by step throughout my career. Thanks to his vision, dedication, and tireless work, I made it all the way to the EuroLeague — something I never thought possible. I will always be deeply grateful to him for believing in me.",
     },
   ],
